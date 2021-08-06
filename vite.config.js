68e72a2f55a69a4ab661425bdf6783ae7ad1ff38
gangashman <ganagsh@gmail.com>
2021-08-06 13:47:00 +0300

    Trigger rebuild

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), svgLoader()]
+  plugins: [vue(), svgLoader()],
+  base: '/bq-frontend/',
 })
